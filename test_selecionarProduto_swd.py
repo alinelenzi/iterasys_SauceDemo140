4886d0fb9b80a4722a55bd3a37290aa825eecad2
Aline Lenzi <alinelenzi@gmail.com>
2024-03-15 14:49:04 -0300

    adicionamos mais passos no arquivo final swd.py

--- test_selecionarProduto_swd.py
+++ test_selecionarProduto_swd.py
@@ -18,7 +18,15 @@ class Test_Produtos():
         # diferença entre quit (matou, só nascendo de novo)/ close (não morre, sai para almoço, volto logo)
         self.driver.quit();                                             # encerra/ destroi o objeto do Selenium WebDriver da memória
 
-    def test_selecionar_produto(self):                                              # método teste
-        self.driver.get(self.url)                                                   #abre navegador
-        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")     #colando o name
-        self.driver.find_element(By.ID, "password").send_keys("secret_sauce")       #colando a senha
+    def test_selecionar_produto(self):                                                                   # método teste
+        self.driver.get(self.url)                                                                        #abre navegador
+        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")                          #colando o name
+        self.driver.find_element(By.ID, "password").send_keys("secret_sauce")                            #colando a senha
+        self.driver.find_element(By.CSS_SELECTOR, "input.submit-button.btn_action").click()               #clique no botão login
+
+        #transição de página
+
+        assert self.driver.find_element(By.CSS_SELECTOR, ".title").text == "Products"                 #verifica se está escrito Products 
+        assert self.driver.find_element(By.ID, "item_4_title_link").text == "Sauce Labs Backpack"        #verifica se está escrito
+        #control+f .nome_da_classe_pai:nth-child(1).nome_da_classe_q_quero - ChroPath
+        assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item:nth-child(1) .inventory_item_price").text == "$29.99"
